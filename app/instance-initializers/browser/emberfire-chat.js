e8209eb2d5e4b8a9cea3b8b54c8d12acae0dca25
Prince_Corn <konradlippertzajaczkowski@gmail.com>
2017-05-11 19:58:06 -0500

    setup

---
+++ app/instance-initializers/browser/emberfire-chat.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from '../../config/environment';
+
+export function initialize( appInstance ) {
+  if (navigator.serviceWorker && navigator.serviceWorker.ready) {
+    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+      // serviceWorkerRegistration.active.postMessage([location.protocol, '//', location.host, location.pathname].join(''));
+    });
+  }
+}
+
+export default {
+  name: 'emberfire-chat',
+  initialize
+};
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* eslint-env node */
 'use strict';
 
+var filterInitializers = require('fastboot-filter-initializers');
+
 module.exports = {
-  name: 'emberfire-chat'
+  name: 'emberfire-chat',
+
+  preconcatTree(tree) {
+    return filterInitializers(tree, this.app.name);
+  }
 };
