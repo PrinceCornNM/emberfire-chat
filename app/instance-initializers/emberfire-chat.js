5185e6f9ee7ab399a859ab5e08a51d8c603c867b
Prince_Corn <konradlippertzajaczkowski@gmail.com>
2017-06-01 21:15:26 -0500

    FASTBOOT: preparation for fastboot 1.0

--- app/instance-initializers/emberfire-chat.js
+++ app/instance-initializers/emberfire-chat.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
-import config from '../../config/environment';
+import config from '../config/environment';
 
 export function initialize( appInstance ) {
-  if (navigator.serviceWorker && navigator.serviceWorker.ready) {
+  if (typeof FastBoot === 'undefined' && navigator.serviceWorker && navigator.serviceWorker.ready) {
     navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
       // serviceWorkerRegistration.active.postMessage([location.protocol, '//', location.host, location.pathname].join(''));
     });
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 /* eslint-env node */
 'use strict';
 
-var filterInitializers = require('fastboot-filter-initializers');
-
 module.exports = {
   name: 'emberfire-chat',
 
-  preconcatTree(tree) {
-    return filterInitializers(tree, this.app.name);
+  isDevelopingAddon: function() {
+    return true;
   }
 };
