346336bd14d293353136ba240fcdda4d33f0ee77
kabirnbaum <kylewdgb69@gmail.com>
2017-07-06 11:25:41 -0500

    CHAT: made it so the chat will always scroll to the most recent message

--- addon/components/firebase-chat.js
+++ addon/components/firebase-chat.js
@@ -13,9 +13,19 @@ const {
 export default Component.extend({
   layout,
   store: service(),
-  newMessages: Ember.observer('messages', function() {
-    $(".chat").scrollTop = $(".chat").scrollHeight;
+  //when a new message arrives reset the chat to the bottom
+  newMessage: Ember.observer('messages', function() {
+    setTimeout(() => {
+      $('.chat').scrollTop($('.chat')[0].scrollHeight)
+    }, 10);
   }),
+  // once page renders scroll to most recent chats
+  didInsertElement : function(){
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, function(){
+      $('.chat').scrollTop($('.chat')[0].scrollHeight);
+    });
+  },
   actions: {
     sendMessage() {
       get(this, 'sendMessage')(get(this, 'messageText'));
