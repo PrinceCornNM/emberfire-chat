e1989fa07fef3e265e37deb0be8fa37ab1c073b7
kabirnbaum <kylewdgb69@gmail.com>
2017-07-17 22:34:50 -0500

    CHAT, CSS: gave chat sample css for testing

--- addon/components/firebase-chat.js
+++ addon/components/firebase-chat.js
@@ -30,44 +30,6 @@ export default Component.extend({
     sendMessage() {
       get(this, 'sendMessage')(get(this, 'messageText'));
     }
-  },
-
-  // contacts: computed(function() {
-    // let store = get(this, 'store');
-
-    // let uid = get(this, 'userId');
-
-    // let contactArr = [];
-    // let contacts = store.query('contacts', { filter: { i: String(uid) } }).then(function(contact) {
-    //   window.contact= contact;
-    //   console.log(contact.get('r'));
-    //   let user = store.findRecord('user', get(contact, 'i'));
-    //   let name = user.n;
-    //   contactArr.push(name);
-    //   console.log(name);
-    // });
-
-    // console.log(7);
-    // let contacts = store.query('contacts', {
-    //   orderBy: 'i',
-    //   equalTo: uid
-    // }).then((contact) => {
-    //   contactArr.push(store.findRecord('user', get(contact, 'r')));
-    // });
-
-
-    // for (var contact in contacts){
-    //   console.log(get(contact, 'r'));
-    //   store.findRecord('user', 'KWfG4jWnaddacnak2dPjR3ox1Gk2');
-    // }
-
-    // contacts.forEach((contact) => {
-    //   console.log(get(get(contact, 'r'), 'id'));
-    // });
-
-    //console.log(contacts);
-
-  //   return contactArr;
-  // })
+  }
 });
 
