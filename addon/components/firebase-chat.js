4da724c6211c243a3547d0826b29bc691a902956
Prince_Corn <konradlippertzajaczkowski@gmail.com>
2017-07-22 15:58:44 -0500

    SCSS, JS, EMBLEM: fixed things

--- addon/components/firebase-chat.js
+++ addon/components/firebase-chat.js
@@ -4,25 +4,25 @@ import layout from '../templates/components/firebase-chat';
 const {
   Component,
   get,
-  set,
   inject: { service },
-  computed,
-  observer
+  observer,
+  run: { scheduleOnce },
+  $
 } = Ember;
 
 export default Component.extend({
   layout,
   store: service(),
-  //when a new message arrives reset the chat to the bottom
-  newMessage: Ember.observer('messages', function() {
+  // when a new message arrives reset the chat to the bottom
+  newMessage: observer('messages', function() {
     setTimeout(() => {
       $('.cht').scrollTop($('.cht')[0].scrollHeight)
     }, 10);
   }),
   // once page renders scroll to most recent chats
-  didInsertElement : function(){
+  didInsertElement: function() {
     this._super();
-    Ember.run.scheduleOnce('afterRender', this, function(){
+    scheduleOnce('afterRender', this, function() {
       $('.cht').scrollTop($('.cht')[0].scrollHeight);
     });
   },
